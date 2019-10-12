668217dfc79f648266ceacfafc29cccd38122670
davidfekke <david@fekke.com>
2019-10-12 15:19:24 -0400

    Initial Commit

---
+++ JaxAvWx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JaxAvWx
+//
+//  Created by David Fekke on 10/12/19.
+//  Copyright © 2019 David Fekke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
